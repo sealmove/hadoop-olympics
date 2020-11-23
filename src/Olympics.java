044d5b0e55f894554a50ce810550c4d19f1a425c
Stefanos Mandalas <47466000+sealmove@users.noreply.github.com>
2020-11-23 19:49:14 +0200

    first 'good' results

--- src/Olympics.java
+++ src/Olympics.java
@@ -2,6 +2,8 @@
  * Stefanos Mandalas 2022 2017 00107 dit17107 */
 
 import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.StringReader;
 import java.util.StringTokenizer;
 
 import org.apache.hadoop.conf.Configuration;
@@ -16,34 +18,43 @@ import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 import org.apache.hadoop.util.GenericOptionsParser;
 
 public class Olympics {
-  public static class TokenizerMapper 
-       extends Mapper<Object, Text, Text, IntWritable>{
-    
-    private final static IntWritable one = new IntWritable(1);
-    private Text word = new Text();
-      
+  public static class TokenizerMapper
+  extends Mapper<Object, Text, IntWritable, IntWritable> {
     public void map(Object key, Text value, Context context)
     throws IOException, InterruptedException {
-      StringTokenizer itr = new StringTokenizer(value.toString());
-      while (itr.hasMoreTokens()) {
-        word.set(itr.nextToken());
-        context.write(word, one);
+      StringReader stringReader = new StringReader(value.toString());
+      BufferedReader csvReader = new BufferedReader(stringReader);
+      String row;
+      while ((row = csvReader.readLine()) != null) {
+        String[] data = row.split(",");
+
+        String id = data[0];
+        if (id.length() != 0) // id field might be empty
+          id = id.substring(1, data[0].length() - 1); // ex.: "123" -> 123
+        int idInt;
+        try {
+          idInt = Integer.parseInt(id);
+        } catch(NumberFormatException nfe) { idInt = 0; }
+        IntWritable idw = new IntWritable(idInt);
+
+        String medal = data[14];
+        if (medal.length() != 0)
+          medal = data[14].substring(1, data[14].length() - 1);
+        IntWritable medalw = new IntWritable(medal.equals("Gold") ? 1 : 0);
+
+        context.write(idw, medalw);
       }
     }
   }
   
-  public static class IntSumReducer 
-       extends Reducer<Text,IntWritable,Text,IntWritable> {
-    private IntWritable result = new IntWritable();
-
-    public void reduce(Text key, Iterable<IntWritable> values, Context context)
-    throws IOException, InterruptedException {
-      int sum = 0;
-      for (IntWritable val : values) {
-        sum += val.get();
-      }
-      result.set(sum);
-      context.write(key, result);
+  public static class IntSumReducer
+  extends Reducer<IntWritable, IntWritable, IntWritable, IntWritable> {
+    public void reduce(IntWritable key, Iterable<IntWritable> values,
+    Context context) throws IOException, InterruptedException {
+      int golds = 0;
+      for (IntWritable val : values)
+        golds += val.get();
+      context.write(key, new IntWritable(golds));
     }
   }
 
@@ -55,17 +66,16 @@ public class Olympics {
       System.err.println("Usage: olympics <in> [<in>...] <out>");
       System.exit(2);
     }
-    Job job = new Job(conf, "word count");
+    Job job = new Job(conf, "Gold medal count");
     job.setJarByClass(Olympics.class);
     job.setMapperClass(TokenizerMapper.class);
     job.setCombinerClass(IntSumReducer.class);
     job.setReducerClass(IntSumReducer.class);
-    job.setOutputKeyClass(Text.class);
+    job.setOutputKeyClass(IntWritable.class);
     job.setOutputValueClass(IntWritable.class);
     job.setJarByClass(Olympics.class);
-    for (int i = 0; i < otherArgs.length - 1; ++i) {
+    for (int i = 0; i < otherArgs.length - 1; ++i)
       FileInputFormat.addInputPath(job, new Path(otherArgs[i]));
-    }
     FileOutputFormat.setOutputPath(job,
       new Path(otherArgs[otherArgs.length - 1]));
     System.exit(job.waitForCompletion(true) ? 0 : 1);
