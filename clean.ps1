05dfa0e25243a9283e08f2a3e5b40a1e7cc76f8c
Stefanos Mandalas <47466000+sealmove@users.noreply.github.com>
2020-11-29 03:13:24 +0200

    deleted 2 scripts

--- clean.ps1
+++
@@ -1,8 +0,0 @@
-<# Anastasia Tsilepi 2022 2015 00179 dit15179
- # Stefanos Mandalas 2022 2017 00107 dit17107 #>
-
-. .\conf.ps1
-
-Remove-Item -ErrorAction Ignore -Recurse $target
-Remove-Item -ErrorAction Ignore *.jar
-Remove-Item -ErrorAction Ignore *.csv
